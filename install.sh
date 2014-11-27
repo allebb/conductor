8869328664d4939b04637c37ee2e6a64fa6dbcf1
Matt Smith <matt@matt40k.co.uk>
2014-11-27 01:17:38 +0000

    Added bugfix and make more generic
    Added bugfix to delete old version
    Made it a bit more generic, ie support for other distros and changing
    the download site

--- install.sh
+++ install.sh
@@ -5,9 +5,19 @@
 # Written by Bobby Allen <ballen@bobbyallen.me>, 22/07/2014                    #
 ################################################################################
 
-VER=$(lsb_release -sr)
+# We define our mirror site
+SITE='https://raw.github.com/bobsta63/conductor/master/scripts'
 
 # We detect what version of OS they are using and then send them off to the correct install script!
-sudo wget https://raw.github.com/bobsta63/conductor/master/scripts/install_ubuntu_$VER.sh
-sudo chmod +x install_ubuntu_$VER.sh
-sudo bash install_ubuntu_$VER.sh
+DISTRO=$(lsb_release -si| tr '[:upper:]' '[:lower:]')
+VER=$(lsb_release -sr)
+INSTALLER='install_'$DISTRO'_'$VER'.sh'
+
+# Check the installer doesn't already exist else we'll end up with install_ubuntu_14.04.sh.1 etc
+if [ -e $INSTALLER ]
+then
+  rm $INSTALLER -f
+fi
+sudo wget $SITE'/'$INSTALLER
+sudo chmod +x $INSTALLER
+sudo bash $INSTALLER
