02222b26d52840b3863142fd33da4b46e109bfbd
Bobby Allen <ballen@bobbyallen.me>
2014-11-28 09:57:13 +0000

    Updated installation script to offer custom installer script base URL as well as removal of existing installer script.

--- install.sh
+++ install.sh
@@ -5,9 +5,19 @@
 # Written by Bobby Allen <ballen@bobbyallen.me>, 22/07/2014                    #
 ################################################################################
 
-VER=$(lsb_release -sr)
+# The installation script repository
+SITE='https://raw.github.com/bobsta63/conductor/master/scripts'
 
-# We detect what version of OS they are using and then send them off to the correct install script!
-sudo wget https://raw.github.com/bobsta63/conductor/master/scripts/install_ubuntu_$VER.sh
-sudo chmod +x install_ubuntu_$VER.sh
-sudo bash install_ubuntu_$VER.sh
+# Detect what version of OS they are using and then send them off to the correct install script!
+DISTRO=$(lsb_release -si| tr '[:upper:]' '[:lower:]')
+VER=$(lsb_release -sr)
+INSTALLER='install_'$DISTRO'_'$VER'.sh'
+
+# Check and remove any installers that already exist on the local machine.
+if [ -e $INSTALLER ]
+then
+  rm $INSTALLER -f
+fi
+sudo wget $SITE'/'$INSTALLER
+sudo chmod +x $INSTALLER
+sudo bash $INSTALLER
