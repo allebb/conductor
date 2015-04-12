a6d0335f1f07292707a1207900a9118cd33921ef
Bobby Allen <ballen@bobbyallen.me>
2015-04-12 21:24:11 +0100

    Some development fixes following testing of the 'rollback' functionality.

--- bin/inc/Conductor.php
+++ bin/inc/Conductor.php
@@ -381,14 +381,14 @@ class Conductor extends CliApplication
     {
         $this->appNameRequired();
         if (!file_exist($this->conf->paths->backups . '/rollback_' . $this->appname . '.tar.gz')) {
-            $this->writeln('There is no available rollback snapshot to restore to.');
+            $this->writeln('There is no available rollback snapshot to restore to!');
             $this->endWithError();
         }
 
         mkdir($this->conf->paths->temp . '/rollback_' . $this->appname, 755);
-        $this->call('tar -zxf ' . $this->conf->paths->backups . '/rollback_' . $this->appname . '.tar.gz -C ' . $this->conf->paths->temp . 'rollback_' . $this->appname);
+        $this->call('tar -zxf ' . $this->conf->paths->backups . '/rollback_' . $this->appname . '.tar.gz -C ' . $this->conf->paths->temp . '/rollback_' . $this->appname);
 
-        if (file_exists($this->conf->paths->temp . 'rollback_' . $this->appname . '/appdb.sql.gz')) {
+        if (file_exists($this->conf->paths->temp . '/rollback_' . $this->appname . '/appdb.sql.gz')) {
             $this->writeln('Importing application MySQL database...');
             $this->call('gunzip < ' . $this->conf->paths->temp . '/rollback_' . $this->appname . '/appdb.sql.gz\' | mysql -h' . $this->conf->mysql->host . ' -u' . $this->conf->mysql->username . ' -p' . $this->conf->mysql->password . ' db_' . $this->appname . '');
             $this->writeln('Finished importing the MySQL database!');
