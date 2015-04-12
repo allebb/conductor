d5bbe521e2dd98e53a09ce2f84dfcf35bc96bbd9
Bobby Allen <ballen@bobbyallen.me>
2015-04-12 14:57:15 +0100

    Dev testing fixes.

--- bin/conductor.php
+++ bin/conductor.php
@@ -10,68 +10,76 @@ $conductor = new Conductor($argv);
 $conductor->enforceCli();
 $conductor->checkDependencies();
 
-switch ($conductor->commands()[1]) {
-    case "list":
-        $conductor->writeln('Applications hosted on this server:');
-        $conductor->listApplications();
-        break;
-    case "new":
-        $conductor->newApplication();
-        break;
-    case "destroy":
-        $conductor->destroy();
-        break;
-    case "update":
-        $conductor->updateApplication();
-        break;
-    case "rollback":
-        $conductor->rollback();
-        break;
-    case "backup":
-        $conductor->backup();
-        break;
-    case "restore":
-        $conductor->restore();
-        break;
-    case "start":
-        $conductor->startLaravelApplication();
-        break;
-    case "stop":
-        $conductor->stopLaravelApplication();
-        break;
-    case "services":
-        $conductor->serviceControl($conductor->commands()[2]);
-        break;
+$commands = $conductor->commands();
 
-    default:
+if (isset($commands[1])) {
+    switch ($commands[1]) {
+        case "list":
+            $conductor->writeln('Applications hosted on this server:');
+            $conductor->listApplications();
+            break;
+        case "new":
+            $conductor->newApplication();
+            break;
+        case "destroy":
+            $conductor->destroy();
+            break;
+        case "update":
+            $conductor->updateApplication();
+            break;
+        case "rollback":
+            $conductor->rollback();
+            break;
+        case "backup":
+            $conductor->backup();
+            break;
+        case "restore":
+            $conductor->restore();
+            break;
+        case "start":
+            $conductor->startLaravelApplication();
+            break;
+        case "stop":
+            $conductor->stopLaravelApplication();
+            break;
+        case "services":
+            $conductor->serviceControl($conductor->commands()[2]);
+            break;
 
-        if ($conductor->isFlagSet('v') or $conductor->isFlagSet('version')) {
-            $conductor->writeln('Conductor v' . $conductor->version());
-            $conductor->endWithSuccess();
-        }
+        default:
 
-        $conductor->writeln();
-        $conductor->writeln('Usage: conductor [OTPION]');
-        $conductor->writeln('');
-        $conductor->writeln('Options:');
-        $conductor->writeln('  list              List all currently hosted applications');
-        $conductor->writeln('  new {name}        Prepares and deploys a new application');
-        $conductor->writeln('  destroy {name}    Removes an application from the server');
-        $conductor->writeln('  update {name}     Upgrades an application via. Git');
-        $conductor->writeln('  rollback {name}   Rolls back the most recent upgrade');
-        $conductor->writeln('  depupdate {name}  Updates all application dependencies (composer update)');
-        $conductor->writeln('  backup {name}     Backs up an application and dependent DB\'s');
-        $conductor->writeln('  restore {name}    Restores an application and dependent DB\'s');
-        $conductor->writeln('  start {name}      Starts a specific application (Laravel Apps only!)');
-        $conductor->writeln('  stop {name}       Stops a specific application (Laravel Apps only!)');
-        $conductor->writeln('  services start    Starts all dependent Conductor daemons');
-        $conductor->writeln('  services stop     Stops all dependent Conductor daemons');
-        $conductor->writeln('  services status   Displays the current daemon statuses');
-        $conductor->writeln('  services restart  Restarts all dependent Conductor daemons');
-        $conductor->writeln('  services reload   Reloads all dependent Conductor daemons');
-        $conductor->writeln('  -v                Displays the version number of Conductor');
-        $conductor->writeln('  -h                Displays this help screen.');
-        $conductor->writeln('');
-        $conductor->writeln('Please report bug at: https://github.com/bobsta63/conductor');
-        $conductor->writeln();
+            if ($conductor->isFlagSet('v') or $conductor->isFlagSet('version')) {
+                $conductor->writeln('Conductor v' . $conductor->version());
+                $conductor->endWithSuccess();
+            }
+            displayHelp();
+    }
+}
+
+function displayHelp($conductor)
+{
+    $conductor->writeln();
+    $conductor->writeln('Usage: conductor [OTPION]');
+    $conductor->writeln('');
+    $conductor->writeln('Options:');
+    $conductor->writeln('  list              List all currently hosted applications');
+    $conductor->writeln('  new {name}        Prepares and deploys a new application');
+    $conductor->writeln('  destroy {name}    Removes an application from the server');
+    $conductor->writeln('  update {name}     Upgrades an application via. Git');
+    $conductor->writeln('  rollback {name}   Rolls back the most recent upgrade');
+    $conductor->writeln('  depupdate {name}  Updates all application dependencies (composer update)');
+    $conductor->writeln('  backup {name}     Backs up an application and dependent DB\'s');
+    $conductor->writeln('  restore {name}    Restores an application and dependent DB\'s');
+    $conductor->writeln('  start {name}      Starts a specific application (Laravel Apps only!)');
+    $conductor->writeln('  stop {name}       Stops a specific application (Laravel Apps only!)');
+    $conductor->writeln('  services start    Starts all dependent Conductor daemons');
+    $conductor->writeln('  services stop     Stops all dependent Conductor daemons');
+    $conductor->writeln('  services status   Displays the current daemon statuses');
+    $conductor->writeln('  services restart  Restarts all dependent Conductor daemons');
+    $conductor->writeln('  services reload   Reloads all dependent Conductor daemons');
+    $conductor->writeln('  -v                Displays the version number of Conductor');
+    $conductor->writeln('  -h                Displays this help screen.');
+    $conductor->writeln('');
+    $conductor->writeln('Please report bug at: https://github.com/bobsta63/conductor');
+    $conductor->writeln();
 }
