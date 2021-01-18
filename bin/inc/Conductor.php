d52be316ddb14e14204867c9bbb6699f11cfd9c0
Bobby Allen <ballen@bobbyallen.me>
2021-01-18 12:24:02 +0000

    tar context fix.

--- bin/inc/Conductor.php
+++ bin/inc/Conductor.php
@@ -193,7 +193,7 @@ class Conductor extends CliApplication
             $this->call('cp ' . $appconf . ' ' . $this->conf->paths->temp . '/' . $this->appname . '/');
         }
         $this->writeln('Compressing backup archive...');
-        $this->call('tar -zcf ' . $this->conf->paths->temp . '/' . $filename . ' -C ' . $this->conf->paths->temp . '/' . $this->appname . '/');
+        $this->call('tar -zcf ' . $this->conf->paths->temp . '/' . $filename . ' -C ' . $this->conf->paths->temp . '/' . $this->appname . '/ .');
         $this->writeln('Cleaning up...');
         $this->call('rm -Rf ' . $this->conf->paths->temp . '/' . $this->appname);
         $this->call('mv ' . $this->conf->paths->temp . '/' . $filename . ' ' . $this->conf->paths->backups . '/' . $filename);
