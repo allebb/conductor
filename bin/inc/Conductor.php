2a703aad411526e35f7ae3be082d60927deb4107
Bobby Allen <ballen@bobbyallen.me>
2021-01-18 12:05:44 +0000

    Pushes a fix for removal of compressed database backup.

--- bin/inc/Conductor.php
+++ bin/inc/Conductor.php
@@ -195,7 +195,7 @@ class Conductor extends CliApplication
         $this->writeln('Compressing backup archive...');
         $this->call('tar - zcf ' . $this->conf->paths->temp . ' / ' . $filename . ' - C ' . $this->conf->paths->temp . ' / ' . $this->appname . ' / .');
         $this->writeln('Cleaning up...');
-        $this->call('rm - Rf ' . $this->conf->paths->temp . ' / ' . $this->appname);
+        $this->call('rm -Rf ' . $this->conf->paths->temp . ' / ' . $this->appname);
         $this->call('mv ' . $this->conf->paths->temp . ' / ' . $filename . ' ' . $this->conf->paths->backups . ' / ' . $filename);
     }
 
